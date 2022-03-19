76db1b0baa5a71f3ae5f61957416244208cb474c
zhj6422 <820105235@qq.com>
2022-03-19 16:40:46 +0800

    week03

--- week03/nio02/README.md
+++ week03/nio02/README.md
@@ -1,6 +1,13 @@
 # netty-gateway
 
-```
-   
+- HttpInboundServer作为程序的入口，配置ServerBootstrap，将HttpInboundInitializer添加位childrenHandler
 
-```
+- HttpInboundInitializer中，往pipeline中加入Handler
+
+- HttpInboundHandler中，重写channelRead方法，当收到客户端发来的请求时，执行这个方法，将请求交给另一个handler进行处理（HttpOutboundHandler）
+
+- HttpOutboundHandler中，通过一个线程池对请求进行处理，每次从备选的几个url中由router随机选择一个url作为请求url，交给线程池，执行fetchGet方法
+
+  fetchGet方法中，请求url
+
+  对请求返回的数据，封装到一个FullHttpResponse返回，并对结果进行过滤
--- week03/nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
+++ week03/nio02/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
@@ -34,50 +34,12 @@ public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         try {
-            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
             FullHttpRequest fullRequest = (FullHttpRequest) msg;
-//            String uri = fullRequest.uri();
-//            //logger.info("接收到的请求url为{}", uri);
-//            if (uri.contains("/test")) {
-//                handlerTest(fullRequest, ctx);
-//            }
-    
             handler.handle(fullRequest, ctx, filter);
-    
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
             ReferenceCountUtil.release(msg);
         }
     }
-
-//    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
-//        FullHttpResponse response = null;
-//        try {
-//            String value = "hello,kimmking";
-//            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
-//            response.headers().set("Content-Type", "application/json");
-//            response.headers().setInt("Content-Length", response.content().readableBytes());
-//
-//        } catch (Exception e) {
-//            logger.error("处理测试接口出错", e);
-//            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
-//        } finally {
-//            if (fullRequest != null) {
-//                if (!HttpUtil.isKeepAlive(fullRequest)) {
-//                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-//                } else {
-//                    response.headers().set(CONNECTION, KEEP_ALIVE);
-//                    ctx.write(response);
-//                }
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-//        cause.printStackTrace();
-//        ctx.close();
-//    }
-
 }
