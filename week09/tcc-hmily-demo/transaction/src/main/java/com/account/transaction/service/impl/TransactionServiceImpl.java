f576188c2ea4ffb9bddaf6ed9691a0ce6ed93eb3
zhj6422 <98689666+zhj6422@users.noreply.github.com>
2022-05-09 00:31:36 +0800

    week09

---
+++ week09/tcc-hmily-demo/transaction/src/main/java/com/account/transaction/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.account.transaction.service.impl;
+
+import com.account.provider.entity.Account;
+import com.account.provider.service.AccountService;
+import com.account.transaction.service.TransactionService;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.hmily.annotation.HmilyTCC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lw1243925457
+ */
+@Service
+@Slf4j
+public class TransactionServiceImpl implements TransactionService {
+
+    final private AccountService accountService;
+
+    @Autowired(required = false)
+    public TransactionServiceImpl(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    @HmilyTCC(confirmMethod = "confirmOrderStatus", cancelMethod = "cancelOrderStatus")
+    public void transaction() {
+        transactionA();
+        transactionB();
+    }
+
+    private void transactionA() {
+        log.info("============py one dubbo try 执行确认付款接口===============");
+        Account account = new Account();
+        account.setId(1L);
+        account.setUs_wallet(-1L);
+        account.setCny_wallet(7L);
+        accountService.pay(account);
+    }
+
+    private void transactionB() {
+        log.info("============py two dubbo try 执行确认付款接口===============");
+        Account account = new Account();
+        account.setId(2L);
+        account.setUs_wallet(1L);
+        account.setCny_wallet(-7L);
+        accountService.pay(account);
+    }
+
+    public void confirmOrderStatus() {
+        log.info("=========进行订单confirm操作完成================");
+    }
+
+    public void cancelOrderStatus() {
+        log.info("=========进行订单cancel操作完成================");
+    }
+}
